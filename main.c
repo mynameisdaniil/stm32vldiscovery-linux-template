a13ca61613cfaec5dd36d27ff0e20eab951ab45b
Daniil <daniil@daniil-laptop.(none)>
2012-09-06 19:07:39 +0400

    New Makefile and hd44780 test

--- main.c
+++ main.c
@@ -1,46 +1,133 @@
-/**
-  @file    main.c
-  @author  h0rr0rr_drag0n
-  @version V0.0.1
-  @date    22-Aug-2011
-  @brief   Template of Linux project for STM32VLDiscovery
-**/
+#include <stm32f10x.h>
+#include <stm32f10x_gpio.h>
+#include <stm32f10x_rcc.h>
 
-#include "stm32f10x.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
-   @brief  Enable clocking on various system peripheral devices
-   @params None
-   @retval None
-**/
-void RCC_init() {
-  /* enable clocking on Port C */
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-}
+/* Подключаем библиотеку и драйвер GPIO */
+#include "hd44780.h"
+#include "hd44780_stm32f10x.h"
 
-int main(void) {
+void init_lcd(void);
+void delay_microseconds(uint16_t us);
+uint32_t uint32_time_diff(uint32_t now, uint32_t before);
+void hd44780_assert_failure_handler(const char *filename, unsigned long line);
+
+HD44780 lcd;
+HD44780_STM32F10x_GPIO_Driver lcd_pindriver;
+volatile uint32_t systick_ms = 0;
+volatile uint16_t delay_counter = 0;
+
+int main(void)
+{
   GPIO_InitTypeDef GPIOC_init_params;
-
-  RCC_init();
-
-  /* Blue LED sits on PC[8] and Green LED sits on PC[9]*/
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
   GPIOC_init_params.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
-  /* Output maximum frequency selection is 10 MHz.
-     Do not worry that internal oscillator of STM32F100RB
-     works on 8MHz frequency - Cortex-M3 core has a various
-     facilities to carefully tune the frequency for almost
-     peripheral devices.
-  */
   GPIOC_init_params.GPIO_Speed = GPIO_Speed_10MHz;
-  /* Push-pull output.
-     Двухтактный выход.
-  */
   GPIOC_init_params.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOC, &GPIOC_init_params);
-
-  /* Set pins 8 and 9 at PORTC to high level */
   GPIO_SetBits(GPIOC, GPIO_Pin_8);
   GPIO_SetBits(GPIOC, GPIO_Pin_9);
-  
-  while (1) {}
+
+  SysTick_Config(SystemCoreClock/1000);//1ms 
+  delay_microseconds(5);
+  GPIO_ResetBits(GPIOC, GPIO_Pin_9);
+
+  init_lcd();
+
+  hd44780_clear(&lcd);
+  hd44780_write_string(&lcd, "Hello");
+}
+
+void __attribute__ ((weak)) SysTick_Handler(void)
+{
+  if(delay_counter > 0)
+  {
+    delay_counter--;
+  }
+/*   ++systick_ms; */
+}
+void Delay_SysTickHandler(void)  
+{  
+  if(delay_counter > 0)
+  {
+    delay_counter--;
+  }
+}
+
+void init_lcd(void)
+{
+  /* Распиновка дисплея */
+  const HD44780_STM32F10x_Pinout lcd_pinout =
+  {
+    {
+      /* RS        */  { GPIOA, GPIO_Pin_5 },
+      /* ENABLE    */  { GPIOA, GPIO_Pin_4 },
+      /* RW        */  { NULL, 0 },
+      /* Backlight */  { NULL, 0 },
+      /* DP0       */  { NULL, 0 },
+      /* DP1       */  { NULL, 0 },
+      /* DP2       */  { NULL, 0 },
+      /* DP3       */  { NULL, 0 },
+      /* DP4       */  { GPIOA, GPIO_Pin_3 },
+      /* DP5       */  { GPIOA, GPIO_Pin_2 },
+      /* DP6       */  { GPIOA, GPIO_Pin_1 },
+      /* DP7       */  { GPIOA, GPIO_Pin_0 },
+    }
+  };
+
+  /* Настраиваем драйвер: указываем интерфейс драйвера (стандартный),
+     указанную выше распиновку и обработчик ошибок GPIO (необязателен). */
+  lcd_pindriver.interface = HD44780_STM32F10X_PINDRIVER_INTERFACE;
+  /* Если вдруг захотите сами вручную настраивать GPIO для дисплея
+     (зачем бы вдруг), напишите здесь ещё (библиотека учтёт это):
+  lcd_pindriver.interface.configure = NULL; */
+  lcd_pindriver.pinout = lcd_pinout;
+  lcd_pindriver.assert_failure_handler = hd44780_assert_failure_handler;
+
+  /* И, наконец, создаём конфигурацию дисплея: указываем наш драйвер,
+     функцию задержки, обработчик ошибок дисплея (необязателен) и опции.
+     На данный момент доступны две опции - использовать или нет
+     вывод RW дисплея (в последнем случае его нужно прижать к GND),
+     и то же для управления подсветкой. */
+  const HD44780_Config lcd_config =
+  {
+    (HD44780_GPIO_Interface*)&lcd_pindriver,
+    delay_microseconds,
+    hd44780_assert_failure_handler,
+    /* HD44780_OPT_USE_RW */
+  };
+
+  /* Ну, а теперь всё стандартно: подаём тактирование на GPIO,
+     инициализируем дисплей: 16x2, 4-битный интерфейс, символы 5x8 точек. */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  hd44780_init(&lcd, HD44780_MODE_4BIT, &lcd_config, 16, 4, HD44780_CHARSIZE_5x8);
+}
+
+void delay_microseconds(uint16_t us)
+{
+  delay_counter = us;
+  while(delay_counter != 0);
+  /* SysTick->VAL = SysTick->LOAD; */
+  /* const uint32_t systick_ms_start = systick_ms; */
+
+  /* while (1) */
+  /* { */
+  /*   uint32_t diff = uint32_time_diff(systick_ms, systick_ms_start); */
+
+  /*   if (diff >= ((uint32_t)us / 1000) + (us % 1000 ? 1 : 0)) */
+  /*     break; */
+  /* } */
+}
+
+uint32_t uint32_time_diff(uint32_t now, uint32_t before)
+{
+  return (now >= before) ? (now - before) : (UINT32_MAX - before + now);
+}
+
+void hd44780_assert_failure_handler(const char *filename, unsigned long line)
+{
+  (void)filename; (void)line;
+  do {} while (1);
 }
